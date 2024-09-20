1b2f40452ac0e56a3078cf89fab0337515fc255b
petersamokhin <contact@petersamokhin.com>
2024-09-20 20:07:07 +0100

    Issue showcase

--- setup.sql
+++ setup.sql
@@ -1,6 +1,3 @@
 CREATE DATABASE edgedb;
 CREATE USER edgedb WITH ENCRYPTED PASSWORD 'edgedb';
 ALTER DATABASE edgedb OWNER TO edgedb;
-
--- does not help, just removes the warnings from the postgres logs
--- ALTER USER edgedb WITH SUPERUSER;
